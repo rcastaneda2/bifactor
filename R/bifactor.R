7a027bd5803ed8d8d165951e3b8a506892028f30
Ruben Castaneda <z5rcastaneda@gmail.com>
2016-02-08 18:51:26 -0800

    first branch
    first branch of bifactor package

---
+++ R/bifactor.R
@@ -0,0 +1,14 @@
+
+#' title: Square a number
+#' 
+#' Takes in any numeric value and squares it
+#' @param x A numeric value to be squared
+#' @return The square of the input
+#' @export 
+
+top<-function(mat,n=5){
+  print(mat[1:n,1:n])
+  return(NULL)
+}
+
+
